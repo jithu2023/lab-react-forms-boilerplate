7fd74a572d50483d4311ceb44ffed4b7b19641a5
jithu2023 <jithumon.j@kalvium.community>
2024-01-11 11:13:51 +0530

    updated

---
+++ Formreact/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import RegistrationForm from './components/Form'
+import './App.css'
+
+function App() {
+  return (
+    <div><RegistrationForm/></div>
+  )
+}
+
+export default App
